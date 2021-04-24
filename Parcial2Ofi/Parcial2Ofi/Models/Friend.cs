9e551e04be684bd6cb87ce37201f035cace46069
Rayner Jimmy Tapia Caballero <a2019116944@estudiantes.upsa.edu.bo>
2021-04-24 07:48:44 -0700

    Se adiciono la clase Friend y el Controlador FriendsController

---
+++ Parcial2Ofi/Parcial2Ofi/Models/Friend.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2Ofi.Models
+{
+    public class Friend
+    {
+        [Key]
+        public int FriendId { get; set; }
+        [Required]
+        [StringLength(30,ErrorMessage ="Ingrese un Nombre de 3 a 30 caracteres",MinimumLength =3)]
+        public string Name { get; set; }
+
+        public string NickName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name ="Cumpleaños")]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}",ApplyFormatInEditMode =true)]
+        public DateTime Birthdate { get; set; }
+    }
+}
